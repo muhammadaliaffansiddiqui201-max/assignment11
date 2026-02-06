9437e764924e41049a5a30f7ea3e78978c5d9d73
muhammadaliaffansiddiqui201-max <muhammadaliaffansiddiqui201@gmail.com>
2026-02-06 08:14:42 -0800

    Add files via upload

---
+++ todo app/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWHS8ZdhJqizd0SL7bYraQi5GEgSFttAk",
+  authDomain: "todo-app1-72a07.firebaseapp.com",
+  projectId: "todo-app1-72a07",
+  storageBucket: "todo-app1-72a07.firebasestorage.app",
+  messagingSenderId: "25213059726",
+  appId: "1:25213059726:web:e6d70ba9ebc5c74e1a7fdf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
