c41f1b0657a6bb6a4ea2132fab90d17ea7f32961
muhammadaliaffansiddiqui201-max <muhammadaliaffansiddiqui201@gmail.com>
2026-02-14 02:27:33 -0800

    Clean repo to project files only

--- assignment 12/script.js
+++
@@ -1,7 +0,0 @@
-
-alert("Welcome to my website!");
-alert("Error! Please enter a valid password.");
-alert("Welcome to JS Land\nHappy Coding!");
-alert("Welcome to JS Land");
-alert("Happy Coding!");
-console.log("Hello... I can run JS through my web browser's console");
